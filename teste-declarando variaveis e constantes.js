59251f87a27decd7371873c316f39bed7d4d4cf7
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2022-06-20 10:51:15 -0300

    adiciona slides das aulas 2 e 3 sobre variáveis, constantes, funções e uso do console no browser e no terminal com node.js

---
+++ teste-declarando variaveis e constantes.js
@@ -0,0 +1,17 @@
+// Declarando variáveis e constantes
+
+// comentário de linha
+// var y = 0;
+
+/* comentário de várias linhas
+var x = 1;
+*/
+
+// var preco = 2;
+var desconto = 0.2;
+
+preco = preco - desconto;
+
+const PRECO = 2;
+
+var total = PRECO - desconto;
---
+++ teste-funcoes.js
@@ -0,0 +1,8 @@
+function soma(a, b) {
+    console.log(a + b);
+    return a + b;
+}
+
+soma();
+
+soma(4, 8);
